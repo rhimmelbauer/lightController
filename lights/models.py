3f4426ae283c6cd7f29aa75bdbd18015f0fbd2b9
rhimmelbauer <roberto.himmelbauer@codeandmodules.com>
2017-11-30 18:43:27 -0800

    home ready

--- lights/models.py
+++ lights/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Zone(models.Model):
+    name = models.CharField(max_length = 100)
+    description = models.CharField(max_length = 100, blank=True, null=True)
+    def __str__(self):
+        return self.name
+
+    
+class Bulb(models.Model):
+    name = models.CharField(max_length = 100)
+    bulb_type = models.CharField(max_length = 100, blank=True, null=True)
+    zone_id = models.ForeignKey(Zone, related_name = 'b_zone_id', blank=True, null=True)
+    onOff = models.BooleanField(default = False)
+    rgbColor = models.IntegerField(default = 0)
+    brightness = models.IntegerField(default = 0)
+    def __str__(self):
+        return self.name
--- lights/views.py
+++ lights/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import *
+
+def home(request):
+    zones = Zone.objects.all()
+    bulbs = Bulb.objects.all()
+    bulbsNoZone = Bulb.objects.exclude(zone_id__isnull=False)
+    return render(request, 'home.html', {'bulbs': bulbs, 'bulbsNoZone': bulbsNoZone, 'zones': zones})
+
+def new_zone(request):
+    
+    return render(request, 'new_component.html')
+
+def edit_bulb(request, pk):
+    bulb = get_object_or_404(Bulb, pk=pk)
+    return render(request, 'new_component.html',{'bulb': bulb})
+
+def control_zone(request, pk):
+    zone = get_object_or_404(Zone, pk=pk)
+    return render(request, 'new_component.html',{'zone': zone})
